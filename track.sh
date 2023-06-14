a3fa456e28a8fc9b0720e230039083c3f8e3f7b8
Preston Pan <preston@nullring.xyz>
2023-06-14 16:30:24 -0700

    add content to journal and mindmap

--- track.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-cp ~/.config/doom/config.org config/doom.org
-cp ~/.config/fish/config.org config/fish.org
-cp ~/org/elfeed.org config/elfeed.org
-cp ~/.config/qtile/config.org config/qtile.org
-cp ~/.config/qutebrowser/config.org config/qutebrowser.org
-cp ~/.config/kmonad/config.org config/kmonad.org
